89abe481e034b68830ccaf16220b9e8d6bfaa6ab
Van Sy <vansy151101@gmail.com>
2024-03-13 14:52:05 +0700

    next time run this file will be auto continue at last item

--- test_generate.py
+++ test_generate.py
@@ -4,12 +4,11 @@ import asyncio
 import json
 import time
 
-limit_question_to_generate_explanation = 1
+limit_question_to_generate_explanation = 200
 question_with_explantion_filename = "question_with_explantion.json"
 question_empty_explantion_filename = "question_empty_explanation.json"
 prompt_filename = "src/prompts/prompt.txt"
 records_empty_explanation = []
-records_explanation = []
 
 with open(prompt_filename, 'r') as f:
     persona_n_prompt = f.read()
@@ -26,11 +25,10 @@ def save_to_jsonl(data, filename):
             json.dump(item, f, ensure_ascii=False)
             f.write('\n')
 
-def append_to_jsonl(data, filename):
+def append_to_jsonl(item, filename):
     with open(filename, 'a') as f:
-        for item in data:
-            json.dump(item, f, ensure_ascii=False)
-            f.write('\n')
+        json.dump(item, f, ensure_ascii=False)
+        f.write('\n')
 
 print(f'Number of objects to generate explanation: {len(records_empty_explanation)}')
 print(f'Limit of number question to generate explanation: {limit_question_to_generate_explanation}')
@@ -41,15 +39,12 @@ for i in range(limit_question_to_generate_explanation):
         explanation = asyncio.run(generate_explanation(records_empty_explanation[i]['question'], persona_n_prompt=persona_n_prompt))
         if explanation != '':
             print(f'Generate successful explanation for question {i} in {time.time() - start_time:.4f}s')
+            records_empty_explanation[i]['explanation'] = explanation
+            append_to_jsonl(records_empty_explanation[i], question_with_explantion_filename)
             del records_empty_explanation[i]
 
     except Exception as e:
         print(e)
-    
-    if explanation and explanation != '':
-        records_empty_explanation[i]['explanation'] = explanation
-        records_explanation.append(records_empty_explanation[i])
 
 
 save_to_jsonl(records_empty_explanation, question_empty_explantion_filename)
-append_to_jsonl(records_explanation, question_with_explantion_filename)
