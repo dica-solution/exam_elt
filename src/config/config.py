381163bec4a9cc6ea7e88ba88dfd10c58ec50bcf
iamtun <letuan19431791.iuh@gmail.com>
2024-03-25 13:21:37 +0700

    Remove unused database and API variables

--- src/config/config.py
+++ src/config/config.py
@@ -5,12 +5,9 @@ class Settings(BaseSettings):
     database_url: Optional[str]
     db_id_mapping: Optional[str]
     db_destination: Optional[str]
-    db_dev_quiz: Optional[str]
     api_authentication_token: Optional[str]
     api_get_by_exam_id: Optional[str]
     api_get_by_page: Optional[str]
-    api_get_quiz_by_id: Optional[str]
-    api_get_filtered_quiz_ids: Optional[str]
 
     model_config = SettingsConfigDict(env_file=".env.dev", env_file_encoding='utf=8', extra='ignore')
 
