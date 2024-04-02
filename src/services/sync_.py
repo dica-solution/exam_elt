e889b21eabe0f30aac429163b30b6834b62fc24a
Van Sy <vansy151101@gmail.com>
2024-04-02 09:07:36 +0700

    Improve performance

--- src/services/sync_.py
+++ src/services/sync_.py
@@ -113,60 +113,126 @@ class ExamUpdater(ExamParser):
         if exam_data_update:
             # Check if the exam has been updated
             if self.check_changes(lastest_runtime, exam_data_update.exam.updated_at):
-                
                 # Update exam
                 record_exam = self.session_import.query(Exam).filter(Exam.id == des_exam_id).first()
-                # record_exam.title = exam_data_update.exam.title
-                # record_exam.term = exam_data_update.exam.term
-                # record_exam.description = exam_data_update.exam.description
-                # record_exam.duration = exam_data_update.exam.duration
-                # record_exam.school_year = exam_data_update.exam.school_year
-                # record_exam.subject_id = exam_data_update.exam.subject_id
-                # record_exam.grade_id = exam_data_update.exam.grade_id
-                # record_exam.school_id = exam_data_update.exam.school_id
-                # record_exam.subdivision_id = exam_data_update.exam.subdivision_id
-                # record_exam.checkpoints = exam_data_update.exam.checkpoints
                 record_exam.updated_at = exam_data_update.exam.updated_at
                 self.session_import.commit()
-
-                # Update quiz question groups
-                for quiz_question_group in exam_data_update.quiz_question_group_list:
-                    record_quiz_group = self.session_import.query(QuizQuestionGroup).filter(QuizQuestionGroup.id == quiz_question_group_id_mapping[quiz_question_group.id]).first()
-                    record_quiz_group.original_text = quiz_question_group.original_text
-                    record_quiz_group.parsed_text = quiz_question_group.parsed_text
-                    record_quiz_group.links = quiz_question_group.links
-                    self.session_import.commit()
-
-                # Update quiz questions
-                order = 0
-                for des_question_id, src_info_dict in id_mapping.items():
-                    idx, quiz_info_dict = self.get_index_by_value(exam_data_update.quiz_info_list, src_info_dict)
-                    update_item = exam_data_update.quiz_question_list[idx]
-                    record_quiz_question = self.session_import.query(QuizQuestion).filter(QuizQuestion.id == des_question_id).first()
-                    record_quiz_question.quiz_question_group_id = update_item.quiz_question_group_id
-                    record_quiz_question.original_text = update_item.original_text
-                    record_quiz_question.parsed_text = update_item.parsed_text
-                    record_quiz_question.quiz_type = update_item.quiz_type
-                    record_quiz_question.quiz_options = update_item.quiz_options
-                    record_quiz_question.explanation = update_item.explanation
-                    record_quiz_question.links = update_item.links
-                    record_quiz_question.quiz_answer = update_item.quiz_answer
-                    self.session_import.commit()
-                    logs = TrackingLogs(
-                        src_exam_id = quiz_info_dict.get('src_exam_id'),
-                        src_quiz_object_type = quiz_info_dict.get('src_quiz_object_type'),
-                        src_quiz_question_id = quiz_info_dict.get('src_quiz_question_id'),
-                        src_quiz_question_group_id = quiz_info_dict.get('src_quiz_question_group_id'),
-                        des_exam_id = des_exam_id,
-                        des_quiz_question_id = des_question_id,
-                        des_quiz_question_group_id = update_item.quiz_question_group_id,
-                        task_name = 'update', # "insert", "update" or "delete"
-                        order = order,
+                num_imported_quizzes = self.session_log.query(TrackingLogs).filter(
+                    and_(
+                        TrackingLogs.src_exam_id == src_exam_id,
+                        TrackingLogs.task_name == "insert"
                     )
-                    order += 1
-                    self.session_log.add(logs)
+                ).count()
+                if len(exam_data_update.quiz_question_list) == num_imported_quizzes: 
+                    # Update quiz question groups
+                    for quiz_question_group in exam_data_update.quiz_question_group_list:
+                        record_quiz_group = self.session_import.query(QuizQuestionGroup).filter(QuizQuestionGroup.id == quiz_question_group_id_mapping[quiz_question_group.id]).first()
+                        record_quiz_group.original_text = quiz_question_group.original_text
+                        record_quiz_group.parsed_text = quiz_question_group.parsed_text
+                        record_quiz_group.links = quiz_question_group.links
+                        self.session_import.commit()
+
+                    # Update quiz questions
+                    order = 0
+                    for des_question_id, src_info_dict in id_mapping.items():
+                        idx, quiz_info_dict = self.get_index_by_value(exam_data_update.quiz_info_list, src_info_dict)
+                        update_item = exam_data_update.quiz_question_list[idx]
+                        record_quiz_question = self.session_import.query(QuizQuestion).filter(QuizQuestion.id == des_question_id).first()
+                        record_quiz_question.quiz_question_group_id = update_item.quiz_question_group_id
+                        record_quiz_question.original_text = update_item.original_text
+                        record_quiz_question.parsed_text = update_item.parsed_text
+                        record_quiz_question.quiz_type = update_item.quiz_type
+                        record_quiz_question.quiz_options = update_item.quiz_options
+                        record_quiz_question.explanation = update_item.explanation
+                        record_quiz_question.links = update_item.links
+                        record_quiz_question.quiz_answer = update_item.quiz_answer
+                        self.session_import.commit()
+                        logs = TrackingLogs(
+                            src_exam_id = quiz_info_dict.get('src_exam_id'),
+                            src_quiz_object_type = quiz_info_dict.get('src_quiz_object_type'),
+                            src_quiz_question_id = quiz_info_dict.get('src_quiz_question_id'),
+                            src_quiz_question_group_id = quiz_info_dict.get('src_quiz_question_group_id'),
+                            des_exam_id = des_exam_id,
+                            des_quiz_question_id = des_question_id,
+                            des_quiz_question_group_id = update_item.quiz_question_group_id,
+                            task_name = 'update', # "insert", "update" or "delete"
+                            order = order,
+                        )
+                        order += 1
+                        self.session_log.add(logs)
+                        self.session_log.commit()
+                    return des_exam_id
+                else:
+                    quiz_info_idx = 0
+                    uniqid_idx = 1 # Ignore the first uniqid - exam
+                    logs_list = []
+                    # Register all related IDs for the current exam
+                    uniqid_list = exam_data_update.uniqid_list
+                    self.session_import.add_all(uniqid_list)
+                    self.session_import.commit()
+
+                    # Update exam info (num_quizzes)
+                    des_exam_id = self.session_log.query(TrackingLogs.des_exam_id).filter(
+                        and_(
+                            TrackingLogs.src_exam_id == src_exam_id,
+                            TrackingLogs.task_name == "insert"
+                        )
+                    ).first()[0]
+                    self.session_import.query(Exam).filter(Exam.id == des_exam_id).update({Exam.num_quizzes: len(exam_data_update.quiz_question_list)})
+
+                    # Update exam_id values of question have that des_exam_id is zero
+                    self.session_import.query(ExamQuestion).filter(ExamQuestion.exam_id == des_exam_id).update({ExamQuestion.exam_id: 0})
+
+                    exam = self.session_import.query(Exam).filter(Exam.id == des_exam_id).first()
+                    exam.num_quizzes = len(exam_data_update.quiz_question_list)
+                    self.session_import.commit()
+
+                    # Store all quiz_question_group_list
+                    ref_quiz_groups = dict()
+                    quiz_question_group_list = exam_data_update.quiz_question_group_list
+                    for group in quiz_question_group_list:
+                        group_id = group.id
+                        group.id = None
+                        self.session_import.add(group)
+                        self.session_import.commit()
+                        ref_quiz_groups[group_id] = group.id
+
+                    # Specify realted quizzes and store them
+                    quiz_question_list = exam_data_update.quiz_question_list
+                    for idx, quiz in enumerate(quiz_question_list):
+                        quiz.id = uniqid_list[uniqid_idx].to_uniqid_number()
+                        uniqid_idx += 1
+                        ref_group_id = quiz.quiz_question_group_id
+                        if ref_group_id:
+                            quiz.quiz_question_group_id = ref_quiz_groups[ref_group_id]
+                        quiz_info_dict = exam_data_update.quiz_info_list[quiz_info_idx]
+                        quiz_info_idx += 1
+                        logs_list.append(TrackingLogs(
+                            src_exam_id = quiz_info_dict.get('src_exam_id'),
+                            src_quiz_object_type = quiz_info_dict.get('src_quiz_object_type'),
+                            src_quiz_question_id = quiz_info_dict.get('src_quiz_question_id'),
+                            src_quiz_question_group_id = quiz_info_dict.get('src_quiz_question_group_id'),
+                            des_exam_id = des_exam_id,
+                            des_quiz_question_id = quiz.id,
+                            des_quiz_question_group_id = quiz.quiz_question_group_id,
+                            task_name = 'update', # "insert", "update"
+                            order = idx,
+                        ))
+                    self.session_import.add_all(quiz_question_list)
+                    self.session_import.commit()
+                    self.session_log.add_all(logs_list)
                     self.session_log.commit()
-                return des_exam_id
+
+                    exam_question_list = []
+                    for idx, question in enumerate(quiz_question_list):
+                        exam_question = ExamQuestion(exam_id=des_exam_id,
+                                                     quiz_question_id=question.id,
+                                                     order=idx,
+                                                     is_checkpoint=False)
+                        exam_question_list.append(exam_question)
+                    self.session_import.add_all(exam_question_list)
+                    self.session_import.commit()
+                    return exam.id
             return 0
 
 
