8096d660062cbb914668a59ec3f718708bfa50d6
Van Sy <vansy151101@gmail.com>
2024-07-18 11:27:39 +0700

    feat: Add tables

--- src/models/exam_bank_models.py
+++ src/models/exam_bank_models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, String, Integer, Date, DateTime, func, Text, Numeric, Boolean, BigInteger
+from sqlalchemy import Column, String, Integer, Date, DateTime, Float, func, Text, Numeric, Boolean, BigInteger
 
 from src.database import Base
 
@@ -41,30 +41,28 @@ class ExamQuestion(Base):
 
 class QuizQuestionGroup(Base):
     __tablename__ = 'quiz_question_group'
-    id = Column(Integer, primary_key=True, nullable=False)
-
+    id = Column(BigInteger, primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
-    original_text = Column(Text, nullable=True)
-    parsed_text = Column(Text, nullable=True)
-    links = Column(Text, nullable=True) # {"audio_links": [], "video_links": [], "image_links": []}
-
+    original_text = Column(Text, nullable=False)
+    parsed_text = Column(Text, nullable=False)
+    links = Column(Text, nullable=False)
 
 class QuizQuestion(Base):
     __tablename__ = 'quiz_question'
-    id = Column(Integer, primary_key=True, nullable=False)
-
+    id = Column(BigInteger, primary_key=True, nullable=False)
     created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
-    user_id = Column(BigInteger, default=1, nullable=False)
-    quiz_question_group_id = Column(Integer, nullable=False)
-    original_text = Column(Text, nullable=True)
-    parsed_text = Column(Text, nullable=True)
+    user_id = Column(BigInteger, nullable=False, default=0)
+    quiz_question_group_id = Column(Integer, nullable=False, default=0)
+    original_text = Column(Text, nullable=False)
+    parsed_text = Column(Text, nullable=False)
     quiz_type = Column(Integer, nullable=False)
-    quiz_options = Column(Text, nullable=True) # {"label": "A", "content": "sample content", "is_correct": true}
-    explanation = Column(Text, nullable=True)
-    links = Column(Text, nullable=True) # {"audio_links": [], "video_links": [], "image_links": []}
-    quiz_answer = Column(Text, nullable=True)
+    quiz_options = Column(Text, nullable=True)
+    explanation = Column(Text, nullable=True, default='')
+    links = Column(Text, nullable=False)
+    quiz_answer = Column(Text, nullable=False)
+    level = Column(Integer, nullable=False, default=0)
 
 
 class Uniqid(Base):
@@ -98,3 +96,111 @@ class SyncLogs(Base):
     __tablename__ = 'sync_logs'
     id = Column(Integer, primary_key=True, nullable=False)
     runtime = Column(DateTime, default=datetime.now(), server_default=func.now(), nullable=False)
+
+class Course(Base):
+    __tablename__ = 'course'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
+    published_at = Column(DateTime, nullable=True)
+    title = Column(String(150), nullable=False, default='')
+    banner_url = Column(String(255), nullable=False, default='')
+    description = Column(Text, nullable=False, default='')
+    description_url = Column(String(255), nullable=False, default='')
+    price = Column(Integer, nullable=False, default=0)
+    discount_price = Column(Integer, nullable=False, default=0)
+    lecture_count = Column(Integer, nullable=False, default=0)
+    grade_id = Column(Integer, nullable=False, default=0)
+    subject_id = Column(Integer, nullable=False, default=0)
+    category_id = Column(Integer, nullable=False, default=1)
+    product_id = Column(String(255), nullable=False, default='')
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(150), nullable=False)
+    status = Column(Boolean, nullable=False)
+
+class CourseLecture(Base):
+    __tablename__ = 'course_lecture'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    course_id = Column(BigInteger, nullable=False)
+    lecture_id = Column(BigInteger, nullable=False)
+    parent_id = Column(BigInteger, nullable=False, default=0)
+    published_at = Column(DateTime, default=datetime.now(), server_default=func.now())
+    level = Column(Integer, nullable=False, default=0)
+    is_free = Column(Boolean, nullable=False, default=False)
+    node_type = Column(Integer, nullable=False, default=0)
+    position = Column(Integer, nullable=False, default=0)
+
+class Lecture(Base):
+    __tablename__ = 'lecture'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
+    title = Column(String(512), nullable=False, default='')
+    content_id = Column(BigInteger, nullable=False, default=0)
+    content_type = Column(String(50), nullable=False, default='')
+    icon_url = Column(String(512), nullable=False, default='')
+
+class Theory(Base):
+    __tablename__ = 'theory'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
+    title = Column(String(512), nullable=True)
+    original_text = Column(Text, nullable=False, default='')
+    parsed_text = Column(Text, nullable=False, default='')
+
+class TheoryExample(Base):
+    __tablename__ = 'theory_example'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    theory_id = Column(BigInteger, nullable=False)
+    question_id = Column(BigInteger, nullable=False)
+    position = Column(Integer, nullable=False)
+
+class QuizCollectionGroup(Base):
+    __tablename__ = 'quiz_collection_group'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
+    name = Column(String(50), nullable=False, default='')
+    user_id = Column(BigInteger, nullable=False, default=0)
+    grade_id = Column(Integer, nullable=False, default=0)
+    subject_id = Column(Integer, nullable=False, default=0)
+    quiz_count = Column(Integer, nullable=False, default=0)
+    indicator = Column(Integer, nullable=False, default=0)
+    type = Column(Integer, nullable=False, default=1)
+
+class QuizCollection(Base):
+    __tablename__ = 'quiz_collection'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=func.now())
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=func.now(), server_default=func.now())
+    user_id = Column(BigInteger, nullable=False, default=0)
+    quiz_id = Column(BigInteger, nullable=False, default=0)
+    last_attempt_id = Column(BigInteger, nullable=False, default=0)
+    quiz_collection_group_id = Column(BigInteger, nullable=False, default=0)
+    source = Column(Integer, nullable=False, default=0)
+    e_factor = Column(Float, nullable=False, default=2.5)
+    interval = Column(Integer, nullable=False, default=1)
+    repetition = Column(Integer, nullable=False, default=0)
+    next_review_at = Column(DateTime, nullable=True)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(BigInteger, primary_key=True, nullable=False)
+    name = Column(String(512), nullable=False, default='')
+    url = Column(String(512), nullable=False, default='')
+    title = Column(String(512), nullable=False, default='')
+    caption = Column(Text, nullable=False, default='')
+    mine_type = Column(String(50), nullable=False, default='')
+
+class CourseIDMapping(Base):
+    __tablename__ = 'course_id_mapping'
+    id = Column(Integer, primary_key=True, nullable=False)
+    original_id = Column(Integer, nullable=False)
+    new_id = Column(BigInteger, nullable=False)
+    entity_type = Column(String(150), nullable=False)
+    parent_new_id = Column(BigInteger, nullable=False, default=0)
+    position = Column(Integer, nullable=False, default=0)
